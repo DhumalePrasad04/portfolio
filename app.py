85235432ad6d711194939a967ee2a5c5a5258f0e
dhumale prasad <dhumaleprasad04@gmail.com>
2025-04-17 22:28:30 +0530

    added education section

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 current_path = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css=current_path/"styles"/"main.css"
 resume=current_path/"assets"/"resume.pdf"
-profilepic=""
+profilepic=current_path/"assets"/"profile pic.jpg"
 
 
 page_title="Prasad's Portfolio"
@@ -20,8 +20,8 @@ social_media={
 projects={
     "CoTrue":"https://github.com/DhumalePrasad04/COtrue",
     "CoTrue2.0":"https://github.com/DhumalePrasad04/CoTrue-2.0",
-    "Speech Or Text to Sign converstion": "https://github.com/DhumalePrasad04/Speech-or-Text-to-Sign-conversion",
-    "Stock Analysis and precdection":"https://github.com/DhumalePrasad04/secure-world",
+    "Speech Or Text to Sign conversion": "https://github.com/DhumalePrasad04/Speech-or-Text-to-Sign-conversion",
+    "Stock Analysis and prediction":"https://github.com/DhumalePrasad04/secure-world",
     "local Chatbot":"https://github.com/DhumalePrasad04/Local-Chatbot-jarvis-",
 }
 
@@ -35,5 +35,38 @@ with open(css) as f:
 with open(resume,"rb") as f:
     pdf=f.read()
 
+col1,col2=st.columns(2,gap="small")
+
+with col1:
+    st.image(profilepic,width=250)
+with col2:
+    st.subheader(name)
+    st.write(description)
+    st.download_button(
+        label="Download Resume",
+        data=pdf,
+        file_name=resume.name,
+        mime="application/octet-stream",
+    )
+    st.write("📩",email)
+st.write("#")
+cols=st.columns(len(social_media))
+for index,(platform,link) in enumerate(social_media.items()):
+    cols[index].write(f"[{platform}]({link})")
+
+st.header("EDUCATION")
+st.write(
+    """
+- #### EDUCATION 
+<ul> 
+<li>GRADUATION | REVA UNIVERSITY, B. Tech in Computer Science and Engineering (2022-2026) CGPA: 9.25 (up to 5th Semester).</li>
+<li>SENIOR SECONDARY | Diamond Independent P U College, Bhalki (2020-2022) Percentage: 94.5%.</li>
+<li>HIGHER SECONDARY | Geetha High School Chegunta (2019-2020) CGPA:10.0 </li> 
+</ul>
+
+
+    """,
+    unsafe_allow_html=True
+)
 
 
